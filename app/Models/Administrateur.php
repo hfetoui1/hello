e441ef5790e85b2887fe4269950e6827ccb2d3cd
haykel <helfetoui@dynaren.fr>
2025-04-21 16:18:16 +0200

    creat administration model

---
+++ app/Models/Administrateur.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Administrateur extends Authenticatable
+{
+    
+
+    use HasFactory,Notifiable;
+
+    protected $table = 'administrateur';
+
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+}
---
+++ database/factories/AdministrateurFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Administrateur>
+ */
+class AdministrateurFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nom' => $this->faker->lastName,
+            'prenom' => $this->faker->firstName,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => Hash::make('password'), // hash du mot de passe par défaut
+        ];
+    }
+}
